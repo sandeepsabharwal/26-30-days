717dc6bdd5195363092af3e0d28bea44805cad7d
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 21:24:05 +0530

    Create day29 que2.c

---
+++ day29 que2.c
@@ -0,0 +1,18 @@
+# Find the maximum and minimum element in an array.
+
+#include <stdio.h>
+
+int main() {
+    int arr[] = {3, 7, 2, 9, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int max = arr[0], min = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) max = arr[i];
+        if(arr[i] < min) min = arr[i];
+    }
+
+    printf("Maximum element: %d\n", max);
+    printf("Minimum element: %d\n", min);
+    return 0;
+}
